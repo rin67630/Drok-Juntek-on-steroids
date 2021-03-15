300d7541d010553a064ac6d21737790f560e5cc0
RIN67630 <lazlo.lebrun@googlemail.com>
2021-03-15 21:21:16 +0100

    Update a1_Libs___Vars.ino

--- Software/a1_Libs___Vars.ino
+++ Software/a1_Libs___Vars.ino
@@ -3,24 +3,54 @@
 #include <Wire.h>          // Libs for I2C
 #include <ArduinoOTA.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
+#define UDP_TX_PACKET_MAX_SIZE 128 //increase UDP size
 #include "time.h"
 #include <FS.h>
 #include <ThingerESP32.h>
+#include <ThingerConsole.h>
 #include <EEPROM.h>
+#include <Button2.h>
 
+#ifdef BOARD_IS_WEMOS 
 #include "SSD1306Wire.h"  // from https://github.com/ThingPulse/esp8266-oled-ssd1306/
-
+#define OLED_W 128
+#define OLED_H 64
 #define OLED_SCL   5  // D1 GPIO5 for I2C (Wire) System Clock
 #define OLED_SDA   4  // D2 GPIO4 for I2C (Wire) System Data
 #define OLED_RST   0  //    GPIO0
-#define MOSI      23  //    GPIO for SPI Master Out
-#define MISO      19  //    GPIO for SPI Master In
-#define SCLK      18  //    GPIO for SPI System Clock
+#define ADC_V      36  //Pin SVP  (V)
+#define ADC_I      39  //Pin SVN  (I)
+#define ADC_P        
+#define PWM_V      12 // Pin SD0  (Vpwm)
+#define PWM_I      14 // Pin SD1  (Ipwm) 
+#endif
 
-#define A0        36  //Pin SVP  (V)
-#define A3        39  //Pin SVN  (I)
-#define PWM0      12 // Pin SD0  (Vpwm)
-#define PWM3      14 // Pin SD1  (Ipwm) 
+#ifdef BOARD_IS_TTGO  
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#define TFT_W 160
+#define TFT_H 128
+#define OLED_SCL   22  // GPIO22 for I2C (Wire) System Clock
+#define OLED_SDA   21  // GPIO21 for I2C (Wire) System Data
+#define TFT_BL      4  // Display backlight control pin
+#define SCL          22  // D1 GPIO22 for I2C (Wire) System Clock
+#define SDA          21  // D2 GPIO21 for I2C (Wire) System Data
+#define RST          0   //    GPIO0
+#define MOSI         23  //    GPIO for SPI Master Out
+#define MISO         19  //    GPIO for SPI Master In
+#define SCLK         18  //    GPIO for SPI System Clock
+#define BUTTON_1     35
+#define BUTTON_2     0
+#define ADC_V        32  //Pin ADC4
+#define ADC_I        33  //Pin ADC5
+#define ADC_P        2   //Pin ADC12        
+#define PWM_V        15  // Pin GPIO15  (Vpwm)
+#define PWM_I        13  // Pin GPIO13  (Ipwm) 
+#define TFT_VERMILON 0xFA60 // better orange
+#endif
+
+
 
 #define RecvPin   15 // Pin for IR Receiver
 
@@ -47,6 +77,8 @@ char Time[10];
 char Date[12];
 byte RunMin;
 byte RunHour;
+String Runtime;
+String Message;
 
 byte slice;
 boolean Each6S;
@@ -70,7 +102,6 @@ boolean triglEvent;
 static IPAddress ip;
 byte wifiConnectCounter;
 
-
 struct dashboard {
 // Measures
   float Vout = 0;
@@ -86,34 +117,39 @@ struct dashboard {
   float Iday ;
   float Wday ;
 } dashboard;
+unsigned char dashboard_punning[sizeof(dashboard)];  //  Array of characters as image of the structure for udp xmit/rcv
 
 struct persistance {
-  long  Runtime;
+  long  Runseconds;
   float Whout ;
-  float Ahout ;  
+  float Ahout ;
+  float initial_voltage;  
 } persistance;
+char persistance_punning[sizeof(persistance)];
 
 float load_internal_resistance;
 float source_internal_resistance;
 float delta_current;
 float delta_voltage;
-float initial_voltage;
 float currentInt;
 int   nCurrent;
 float CycleVInt;
 float CycleIInt;
 float CycleWInt;
 int   nCycle; 
+boolean cycleRST;
 
 // Statistics
 float Vh[31];
 float Ah[31];
 float Wh[31];
 
-int A0Raw;
-int A3Raw;
-int lastA0;
-int lastA3;
+int ADC_VRaw;
+int ADC_IRaw;
+int ADC_PRaw;
+int lastADC_V;
+int lastADC_I;
+int lastADC_P;
 
 // Injection PWM output values
 unsigned int CVinj;
