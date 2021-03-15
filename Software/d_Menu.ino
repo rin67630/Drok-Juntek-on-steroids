f2ac483ae05bf3de707662f0f55493b7206eb60f
RIN67630 <lazlo.lebrun@googlemail.com>
2021-03-15 21:23:34 +0100

    Update d_Menu.ino

--- Software/d_Menu.ino
+++ Software/d_Menu.ino
@@ -35,58 +35,71 @@ void menuRun()
       break;
     case '+': //Increase Volt
       //CVinj += 10;
-      //Console1.printf ("+1 Vinj=%i Vadc=%i\n", CVinj, A0Raw);
+      //Console1.printf ("+1 Vinj=%i Vadc=%i\n", CVinj, ADC_VRaw);
       dashboard.Vset += 0.1;
       Console1.printf ("+1 Volt=%06.3f\n", dashboard.Vset);
       break;
     case '-': //Reduce Volt
       //CVinj -= 10;
-      //Console1.printf ("-1 Vinj=%i Vadc=%i\n", CVinj, A0Raw);
+      //Console1.printf ("-1 Vinj=%i Vadc=%i\n", CVinj, ADC_VRaw);
       dashboard.Vset -= 0.1;
       Console1.printf ("-1 Volt=%06.3f\n", dashboard.Vset);
       break;
     case '>': //Increase Amp
       //CCinj += 10;
-      //Console1.printf ("+1 Cinj=%i Iadc=%i\n", CCinj,A3Raw);
+      //Console1.printf ("+1 Cinj=%i Iadc=%i\n", CCinj,ADC_IRaw);
       dashboard.Iset += 0.05;
       Console1.printf ("+10 Amp=%06.3f \n", dashboard.Iset);
       break;
     case '<': //Reduce Amp
       //CCinj -= 10;
-      //Console1.printf ("+1 Cinj=%i Iadc=%i\n", CCinj,A3Raw);
+      //Console1.printf ("+1 Cinj=%i Iadc=%i\n", CCinj,ADC_IRaw);
       dashboard.Iset -= 0.05;
       Console1.printf ("-10 Amp=%06.3f\n", dashboard.Iset);
       break;
-    case 'Z':  //Reset
+    case 'Z':  // Write persistance and Reset
+      for ( int i = 0; i < sizeof(persistance); ++i ) EEPROM.write ( i + 100,  persistance_punning[i] );
+      EEPROM.commit();
+      memcpy(persistance_punning, &persistance, sizeof(persistance));
       Console1.printf ("\nResetting in 10s...\n");
       delay(10000);
       ESP.restart();
       break;
     case 'z':  //Reset runtime
-      Console1.printf ("\nRt:%06i s Ah:%06.3f Wh:%06.3f \n", persistance.Runtime, persistance.Ahout, persistance.Whout);
+      Console1.printf ("\nRt:%06i s Ah:%06.3f Wh:%06.3f \n", persistance.Runseconds, persistance.Ahout, persistance.Whout);
       Console1.printf ("\nResetting \n");
-      persistance.Runtime = 0;
+      persistance.Runseconds = 0;
       persistance.Ahout = 0;
       persistance.Whout = 0;
       CycleVInt = CycleIInt = CycleWInt = nCycle = 0;
-      initial_voltage = dashboard.Vout;
-      break;    
+      persistance.initial_voltage = dashboard.Vout;
+      thing.stream("status"); 
+      break;
 
     // ***One shot Reports**
     case 'S':  //Summary Report
       Console1.printf ("\nSummary Report\n");
       serialPage = 'S';
       break;
+     case 'D':  //Debug Report
+      Console1.printf ("\nDebug Report\n");
+      serialPage = 'D';
+      break;
     case 'P': //Parameter List
       Console1.printf("Par.List \n Vout %6.2f CV %6.2f Iout %6.3f CC %6.3f", dashboard.Vout, dashboard.Vset, dashboard.Iout, dashboard.Iset );
-       break;
+      break;
     case 't': // Print time
       Console1.println(ctime(&now));
       break;
     case 'T': // Enter time
       setTimefromSerial();
       break;
-
+    case 'W': // Write Persistance data to EEPROM (Adress = 100...)
+      for ( int i = 0; i < sizeof(persistance); ++i ) EEPROM.write ( i + 100,  persistance_punning[i] );
+      EEPROM.commit();
+      memcpy(persistance_punning, &persistance, sizeof(persistance));
+      //persistance_punning[i] = EEPROM.read ( i + 100 );
+      break;
     // ***Options for periodical Reports**
     case 'd': //Day Report
       serialPeriodicity = 'd';
@@ -115,7 +128,7 @@ void menuRun()
       serialPage = 'E';
       //      Console1.printf ("\nEnergy plot :\n");
       break;
-    case 'W':  //PWM Plot
+    case 'V':  //Variables (PWM, ADC) tracking
       serialPage = 'W';
       //      Console1.printf ("\nPWM Plot :\n");
       break;
@@ -123,7 +136,7 @@ void menuRun()
       serialPage = '~';
       Console1.printf ("\nWiFi Plot :\n");
       WiFi.printDiag(Console1);
-      Console1.flush();
+      //Console1.flush();
       break;
   } //end switch (inbyte)
 
