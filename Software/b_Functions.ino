2d1c44d283ae714eb4725f223b0fed285f73dc99
RIN67630 <lazlo.lebrun@googlemail.com>
2021-04-15 22:43:36 +0200

    Update b_Functions.ino

--- Software/b_Functions.ino
+++ Software/b_Functions.ino
@@ -8,7 +8,7 @@
 WiFiUDP UDP; // Creation of wifi Udp instance
 
 ThingerESP32 thing(THINGER_USERNAME, THINGER_DEVICE, THINGER_DEVICE_CREDENTIALS);
-ThingerConsole console(thing);
+//ThingerConsole console(thing);
 
 #ifdef BOARD_IS_WEMOS
 SSD1306Wire display(0x3c, OLED_SCL, OLED_SDA);                  //OLED 128*64 soldered
@@ -16,9 +16,11 @@ SSD1306Wire display(0x3c, OLED_SCL, OLED_SDA);                  //OLED 128*64 so
 
 #ifdef BOARD_IS_TTGO
 TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
-#define TFT_GREY 0x5AEB // New colour
+#define TFT_GREY     0x5AEB // better Grey
+#define TFT_VERMILON 0xFA60 // better Orange
 #endif
 
+ADS1115_WE adc(0x48);
 
 
 // Functions
@@ -40,7 +42,7 @@ void getWiFi()
       if (wifiConnectCounter > wifiMaxTries)
       {
         Console4.printf("\n\nBad SSID or PASS?\n");
-        //        WiFi.disconnect();
+        WiFi.disconnect();
         Console4.printf("\nRunning offline, enter time date & time (dd/mm/yyyy hh:mm:ss) before using the menu\n");
         break;
       }
@@ -49,8 +51,8 @@ void getWiFi()
     WiFi.setHostname(DEVICE_NAME);
   }
   ip = WiFi.localIP();
-  Console4.print("RRSI: ");
-  Console4.println(WiFi.RSSI());
+  Console4.print(" Done!\nRRSI= ");   Console4.print(WiFi.RSSI());
+  sprintf(charbuff, "dB, IP= %03d . %03d %03d . %03d \n",  ip[0], ip[1], ip[2], ip[3]);  Console4.printf(charbuff);
 }
 
 /*
@@ -95,60 +97,7 @@ void getTimeData()
 
 void buffTimeData()   // writes the time/date in Charbuff for print or display
 {
-  strftime(charbuff, sizeof(charbuff), "%R %d%b", timeinfo);
-}
-
-void espDelay(int ms)
-{   
-    esp_sleep_enable_timer_wakeup(ms * 1000);
-    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,ESP_PD_OPTION_ON);
-    esp_light_sleep_start();
-}
-
-void setBrightness( int brightness)  // Display brightness 0..2047
-{
-  ledcWrite(14, brightness);
-}
-
-// Other Math / conversions
-bool inRange(int x, int low, int high) // checks if a value is in boundaries
-{  
-  if (x >= low && x <= high)
-    return true;
-  return false;
-}
-
-float adc2vout(unsigned int adc)     //Conversion ADC to Volt
-{
-  float volt = map(adc, ADC0V, 1500, 0, VOUT_1500);
-  volt = volt / 1000;
-  return volt;
-}
-
-float adc2vin(unsigned int adc)     //Conversion ADC to Volt
-{
-  float volt = map(adc, ADC0V, 1500, 0, VIN_1500);
-  volt = volt / 1000;
-  return volt;
-}
-unsigned int  volt2pwm(float volt)     //Conversion Volt to PWM
-{
-  unsigned int pwm = map(volt * 1000, 0, VOUT_1500, PWM_VV, PWM_1500);
-  return pwm;
-}
-
-float adc2amp(unsigned int adc)     //Conversion ADC to Ampere
-{
-
-  float amp = map(adc, ADC0A, 2047, 0, AMP_MAX);
-  amp = amp / 1000;
-  return amp;
-}
-
-unsigned int amp2pwm(float amp)     //Conversion Ampere to PWM
-{
-  unsigned int pwm = map(amp * 1000, 0, AMP_MAX, PWM_VA, PWMAMAX);
-  return pwm;
+  strftime(charbuff, sizeof(charbuff), " %R %d%b ", timeinfo);
 }
 
 void setTimefromSerial()           // Enter time over serial
@@ -186,3 +135,58 @@ void setTimefromSerial()           // Enter time over serial
       }
     }
 }
+
+void espDelay(int ms)
+{   
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,ESP_PD_OPTION_ON);
+    esp_light_sleep_start();
+}
+
+void setBrightness( int brightness)  // Display brightness 0..2047
+{
+  ledcWrite(14, brightness);
+}
+
+// Other Math / conversions
+bool inRange(int x, int low, int high) // checks if a value is in boundaries
+{  
+  if (x >= low && x <= high)
+    return true;
+  return false;
+}
+
+#ifdef ADC_IS_ESP
+float adc2vout(unsigned int adc)     //Conversion ADC to Volt
+{
+  float volt = map(adc, VOUT_0, 1500, 0, VOUT_1500);
+  volt = volt / 1000;
+  return volt;
+}
+
+float adc2vin(unsigned int adc)     //Conversion ADC to Volt
+{
+  float volt = map(adc, VIN_0, 1500, 0, VIN_1500);
+  volt = volt / 1000;
+  return volt;
+}
+
+float adc2amp(unsigned int adc)     //Conversion ADC to Ampere
+{
+  float amp = map(adc, AOUT_0, 2047, 0, AOUT_1500);
+  amp = amp / 1000;
+  return amp;
+}
+
+unsigned int  volt2pwm(float volt)     //Conversion Volt to PWM
+{
+  unsigned int pwm = map(volt * 1000, 0, VOUT_1500, PWM_V0, PWM_V1500);
+  return pwm;
+}
+
+unsigned int amp2pwm(float amp)     //Conversion Ampere to PWM
+{
+  unsigned int pwm = map(amp * 1000, 0, AOUT_1500, PWM_A0, PWM_A1500);
+  return pwm;
+}
+#endif
