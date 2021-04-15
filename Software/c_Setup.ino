072256f8fd1ecd4b5dd19bb209ab6d7c591ba622
RIN67630 <lazlo.lebrun@googlemail.com>
2021-04-15 22:44:07 +0200

    Update c_Setup.ino

--- Software/c_Setup.ino
+++ Software/c_Setup.ino
@@ -5,12 +5,34 @@ void setup()
   Serial.begin (SERIAL_SPEED); // On USB port
   Serial.setDebugOutput(true);
   Wire.begin(OLED_SDA, OLED_SCL);
+  Console4.printf("Resetted! \nDevice name: %s \n", DEVICE_NAME);
+  Console4.printf("ESP-Karajan framework at work, Serial @ %u Baud\nTry connecting to %s ", SERIAL_SPEED, WIFI_SSID);
 
-
+#ifdef ADC_IS_ADS1115
   // Settings for ADC
-  analogSetWidth(11);               // 11Bit resolution
-  analogSetAttenuation(ADC_0db);    // 0=0db (0..1V) 1= 2,5dB; 2=-6dB (0..2V); 3=-11dB  0.2..2.6V ~linear
+  if (not adc.init()) 
+  {
+  Console4.println("ADS1115 not connected!");
+  }else{
+  /* Set the voltage range of the ADC to adjust the gain
+    Please note that you must not apply more than VDD + 0.3V to the input pins!
 
+    ADS1115_RANGE_6144  ->  +/- 6144 mV
+    ADS1115_RANGE_4096  ->  +/- 4096 mV
+    ADS1115_RANGE_2048  ->  +/- 2048 mV (default)
+    ADS1115_RANGE_1024  ->  +/- 1024 mV
+    ADS1115_RANGE_0512  ->  +/- 512 mV
+    ADS1115_RANGE_0256  ->  +/- 256 mV
+
+    //adc.setAlertPinMode(ADS1115_ASSERT_AFTER_1); //uncomment if you want to change the default
+    adc.setAlertPinMode(ADS1115_DISABLE_ALERT); //default
+  */
+  //adc.setVoltageRange_mV(ADS1115_RANGE_2048); //comment line/change parameter to change range
+  //adc.setAutoRange();
+  adc.setConvRate(ADS1115_250_SPS);
+  adc.setMeasureMode(ADS1115_SINGLE); //comment line/change parameter to change mode
+  }
+#endif
 
 
   // Settings for PWM
@@ -22,8 +44,9 @@ void setup()
   ledcAttachPin(TFT_BL, 14);
   //ledcWrite(channel, dutycycle);
 
+  analogSetWidth(11);               // 11Bit resolution
+  analogSetAttenuation(ADC_0db);    // 0=0db (0..1V) 1= 2,5dB; 2=-6dB (0..2V); 3=-11dB  0.2..2.6V ~linear
 
-  Console4.printf("\nESP-Karajan at work,Serial @ %u Baud\nTrying to connect\n\n", SERIAL_SPEED);
 
 #ifdef BOARD_IS_TTGO
   tft.init();
@@ -33,6 +56,11 @@ void setup()
   tft.setTextColor(TFT_WHITE);
   tft.setCursor(0, 0);
   setBrightness (BRIGHTNESS);    // Not part of the tft lib. Function is retrofitted in b_Functions
+  tft.setCursor(0, 0, 2); tft.setTextFont(2);  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.print("Device Name: ");
+  tft.println(DEVICE_NAME);  
+  tft.print("Try connecting..  ");
 #endif
 
 
@@ -54,17 +82,17 @@ void setup()
   delay(50);
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0, 0,  DEVICE_NAME);
-  display.drawString(0, 12, "Try connect..");
+  display.drawString(0, 0,  "Device Name: ");
+  display.drawString(64, 0,  DEVICE_NAME);
+  display.drawString(0, 12, "Try connecting... "); 
   display.display();
 #endif
 
   // Networking and Time
-
   WiFi.mode(WIFI_STA);
 
   getWiFi();
-
+  
   delay(100);
 
   if (WiFi.status() == WL_CONNECTED)
@@ -106,18 +134,26 @@ void setup()
     });
     ArduinoOTA.setHostname(DEVICE_NAME);
     ArduinoOTA.begin();
-    //    Console4.printf("OTA Ready\n MAC address: %s\nHostname: %s \n", WiFi.macAddress().c_str(), WiFi.hostname().c_str());
 
 #ifdef BOARD_IS_WEMOS
-    sprintf(charbuff, "Connected!"); display.drawString(60, 24, charbuff);
-    sprintf(charbuff, "IP= %03d . %03d",  ip[2], ip[3]); display.drawString(0, 36, charbuff);
+    sprintf(charbuff, " Connected!"); display.drawString(60, 12, charbuff);
+    sprintf(charbuff, "IP= %03d . %03d . %03d . %03d", ip[0], ip[1], ip[2], ip[3]); display.drawString(0, 24, charbuff); 
+    display.drawString(0, 36, "User name: ");
+    display.drawString(64, 36, THINGER_USERNAME);
     display.display();
-    delay(2000);
 #endif
-    getNTP();
 
-#if defined (THINGER)
-    Console4.printf(" Initializing Thinger\n");
+#ifdef BOARD_IS_TTGO
+    tft.print(" Connected!\n");
+    sprintf(charbuff, "IP= %03d . %03d %03d . %03d \nUser Name: ",  ip[0], ip[1], ip[2], ip[3]); tft.print(charbuff); tft.println(THINGER_USERNAME);
+ #endif
+
+    delay(2000);
+    getNTP();
+    
+#ifdef THINGER
+    Console4.printf("Initializing Thinger, Username= %s",THINGER_USERNAME);
+    
     // definition of structures for transmission
     // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
     // resource output example (i.e. reading a sensor value) https://docs.thinger.io/coding#define-output-resources
@@ -126,7 +162,11 @@ void setup()
 
 
     // Thing control processing
- 
+    thing["cycleRST"] << [](pson & in)
+    {
+      cycleRST = in;
+    };
+
 
     thing["menu"] << [](pson & in)
     {
@@ -171,8 +211,8 @@ void setup()
       out["Vout"]            = dashboard.Vout ;
       out["Iout"]            = dashboard.Iout ;
       out["Wout"]            = dashboard.Wout ;
-      out["Ahout"]           = dashboard.Ahout ;
-      out["Whout"]           = dashboard.Whout ;
+      out["Ahout"]           = persistance.Ahout ;
+      out["Whout"]           = persistance.Whout ;
       out["Vin"]             = dashboard.Vin ;
       out["Iin"]             = dashboard.Iin ;
       out["Iout"]            = dashboard.Iout ;
@@ -193,8 +233,8 @@ void setup()
       out["Vout"]             = dashboard.Vout ;
       out["Iout"]             = dashboard.Iout ;
       out["Wout"]             = dashboard.Wout ;
-      out["Ahout"]            = dashboard.Ahout ;
-      out["Whout"]            = dashboard.Whout ;
+      out["Ahout"]            = persistance.Ahout ;
+      out["Whout"]            = persistance.Whout ;
       out["Vin"]              = dashboard.Vin ;
       out["Iin"]              = dashboard.Iin ;
       out["Iout"]            = dashboard.Iout ;
@@ -207,8 +247,8 @@ void setup()
       out["Vout"]             = dashboard.Vout ;
       out["Iout"]             = dashboard.Iout ;
       out["Wout"]             = dashboard.Wout ;
-      out["Ahout"]            = dashboard.Ahout ;
-      out["Whout"]            = dashboard.Whout ;
+      out["Ahout"]            = persistance.Ahout ;
+      out["Whout"]            = persistance.Whout ;
       out["Vin"]              = dashboard.Vin ;
       out["Iin"]              = dashboard.Iin ;
       out["Iout"]            = dashboard.Iout ;
@@ -226,18 +266,30 @@ void setup()
     //Communication with Thinger.io
     thing.handle();
     delay(1000); // Wait for contact to happen.
-    
+
 #endif
 
-  }
+  } //end if WiFi Status == Connected
 
   delay(1000);
 
   getEpoch();            // writes the Epoch (Numbers of seconds till 1.1.1970...
   getTimeData();         // breaks down the Epoch into discrete values.
 
-  sprintf(charbuff, "Now is %s, %02d %s %04d %02d:%02d:%02d. Epoch =%10lu\n", DayName, Day, MonthName, Year, Hour, Minute, Second, Epoch);  Console4.println(charbuff);
+  sprintf(charbuff, "\nNow is %s, %02d %s %04d %02d:%02d:%02d. Epoch =%10lu\n", DayName, Day, MonthName, Year, Hour, Minute, Second, Epoch);  Console4.println(charbuff);
+  sprintf(charbuff, "%s, %02d %s %04d %02d:%02d:%02d", DayName, Day, MonthName, Year, Hour, Minute, Second); 
 
+#ifdef BOARD_IS_WEMOS
+    display.drawString(0, 48, charbuff);
+    display.display();
+#endif
+
+#ifdef BOARD_IS_TTGO
+    tft.println();
+    tft.print(charbuff); 
+ #endif
+
+delay(10000);
 
   // Initialisations.
 
@@ -249,18 +301,18 @@ void setup()
 #ifndef THINGER
   // read/write Persistance from EEPROM (Adress = 100...)
   for ( int i = 0; i < sizeof(persistance); ++i ) persistance_punning[i] = EEPROM.read ( i + 100 );
-
   memcpy(&persistance, persistance_punning, sizeof(persistance));
   //  EEPROM.write ( i + 100,  persistance_punning[i] );
 #endif
 
-serialPage = '0';           // default reporting page 
+  serialPage = '0';           // default reporting page
+  serialPeriodicity = '!';
 
   if (dashboard.Vset == 0)   // still uninitialized, loading defaults
   {
     dashboard.Vset = 14.4;
     dashboard.Iset = 0.7;
-    lastADC_V = lastADC_I = 500;
+    lastADC_Vout = lastADC_Iout = 500;
   }
 }
 //end Setup
