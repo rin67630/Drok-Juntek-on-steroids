c1445cc4ca3555afe5188f0ecf1649ae4aba38b8
RIN67630 <lazlo.lebrun@googlemail.com>
2021-04-15 22:29:45 +0200

    Create Credentials.h

---
+++ Software/Credentials.h
@@ -0,0 +1,11 @@
+//  ***Credentials***  
+// WifI: for the very first run with a new ESP or on a new WiFi network 
+// remove the // from the next line and provide real SSID and PASS credentials.
+//#define ERASE_WIFI_CREDENTIALS 
+#define WIFI_SSID          "your SSID"
+#define WIFI_PASS          "your password"
+
+//Thinger
+#define THINGER_USERNAME           "your user name"       
+#define THINGER_DEVICE_CREDENTIALS "your device credentials"  //NOT the website password!
+#define THINGER_DEVICE DEVICE_NAME  
