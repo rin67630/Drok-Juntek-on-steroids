ffe3fc3a0ad6024038f9f7c164c76d10cc65e6b5
RIN67630 <lazlo.lebrun@googlemail.com>
2021-06-18 17:42:40 +0200

    Update h_Wireless.ino

--- Software/h_Wireless.ino
+++ Software/h_Wireless.ino
@@ -9,20 +9,66 @@ void wirelessRun()
 
   MillisMem = millis();
   thing.handle();
-  if (NewMinute) 
+  if (NewMinute)
   {
     thing.stream("measure");
-    thing.stream("status"); 
+    thing.stream("status");
   }
 
 #if defined (WRITE_BUCKETS)
-  if (triglEvent)   thing.write_bucket("EVENT", "EVENT");
-  if (NewDay)       thing.write_bucket("DAY", "DAY");
-  if (HourExpiring) thing.write_bucket("HOUR", "HOUR");
-  if (NewMinute)    thing.write_bucket("MIN", "MIN");
+  if (triglEvent)     thing.write_bucket("EVENT", "EVENT");
+  if (DayExpiring)    thing.write_bucket("DAY", "DAY");
+  if (HourExpiring)   thing.write_bucket("HOUR", "HOUR");
+  if (MinuteExpiring) thing.write_bucket("MIN", "MIN");
 #endif
 
+  if (Minute % 6 == 5)           //every 6 minutes update persistence data
+  {
+    pson Ah_prop;// 0..23=hour, 25=current, 26=Ah 24h, 27= AhDay
+    Ah_prop["00h"] = Ah[0];
+    Ah_prop["01h"] = Ah[1];
+    Ah_prop["02h"] = Ah[2];
+    Ah_prop["03h"] = Ah[3];
+    Ah_prop["04h"] = Ah[4];
+    Ah_prop["05h"] = Ah[5];
+    Ah_prop["06h"] = Ah[6];
+    Ah_prop["07h"] = Ah[7];
+    Ah_prop["08h"] = Ah[8];
+    Ah_prop["09h"] = Ah[9];
+    Ah_prop["10h"] = Ah[10];
+    Ah_prop["11h"] = Ah[11];
+    Ah_prop["12h"] = Ah[12];
+    Ah_prop["13h"] = Ah[13];
+    Ah_prop["14h"] = Ah[14];
+    Ah_prop["15h"] = Ah[15];
+    Ah_prop["16h"] = Ah[16];
+    Ah_prop["17h"] = Ah[17];
+    Ah_prop["18h"] = Ah[18];
+    Ah_prop["19h"] = Ah[19];
+    Ah_prop["20h"] = Ah[20];
+    Ah_prop["21h"] = Ah[21];
+    Ah_prop["22h"] = Ah[22];
+    Ah_prop["23h"] = Ah[23];
+    Ah_prop["LastHour"] = Ah[25];
+    Ah_prop["Yesterday"] = Ah[27];
+    Ah_prop["Today"] = Ah[26];
+    thing.set_property("AhStat", Ah_prop);
 
+    pson LTData;
+    LTData["initial_voltage"] = persistence.initial_voltage;
+    LTData["voltageAt0H"] = persistence.voltageAt0H ;
+    LTData["voltageDelta"] = persistence.voltageDelta ;
+    LTData["HourVSum"] = persistence.HourVSum;
+    LTData["HourISum"] = persistence.HourISum;
+    LTData["HourWSum"] = persistence.HourWSum;
+    LTData["HourSamples"] = persistence.HourSamples;
+    LTData["CycleVSum"] = persistence.CycleVSum;
+    LTData["CycleISum"] = persistence.CycleISum;
+    LTData["CycleWSum"] = persistence.CycleWSum;
+    LTData["CycleSamples"] = persistence.CycleSamples;
+    LTData["AhMode"] = persistence.AhMode;
+    thing.set_property("LongTerm", LTData);
+  }
 #endif // defined Thinger
 
 #if defined (PUBLISH_REPORT)               // Read with NetCat Bash command nc -u -l [UDP_PORT +1]
@@ -40,7 +86,7 @@ void wirelessRun()
         }
         else
         {
-          UDP.printf("%02u  | %+07.3f | %+07.3f | %+07.3f |\n", n, Ah[n], Vbat[n], Ah[n] * Vbat[n]);
+          UDP.printf("%02u  | %+07.3f | %+07.3f | %+07.3f |\n", n, Ah[n], VoutAvg[n], Wh[n]);
         }
       }
     }
@@ -63,12 +109,19 @@ void wirelessRun()
     yield();
 #endif
 
-  pson persistances;
-  persistances["Ah/hour"] = persistance.Ahout;
-  persistances["Wh/hour"] = persistance.Whout;
-  persistances["Runseconds"] = persistance.Runseconds;
-  persistances["InitialVoltage"] = persistance.initial_voltage;
-  thing.set_property("persistances", persistances, true);
-  yield();
+    pson thing_property;
+    //Periodically retrieve the PID parameters, comment out once the PID Paramters are stable
+    thing.get_property ("thing_property", thing_property);
+    P_value = thing_property["_P_value"]; I_value = thing_property["_I_value"]; D_value = thing_property["_D_value"];
+
+    //Periodically save the values that must persist after reboot
+    thing_property["Ah/hour"] = Ahout;
+    thing_property["Wh/hour"] = Whout;
+    thing_property["voltageAt0H"] =  persistence.voltageAt0H;
+    thing_property["voltageDelta"] = persistence.voltageDelta;
+    thing_property["InitialVoltage"] = persistence.initial_voltage;
+    thing_property["HourSamples"] = persistence.HourSamples;
+    thing.set_property("thing_property", thing_property, true);
+    yield();
   }
 }
