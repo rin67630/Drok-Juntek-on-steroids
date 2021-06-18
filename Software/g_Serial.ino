855b59dc6375a864e6dfc4c9ec304e1aad4794e8
RIN67630 <lazlo.lebrun@googlemail.com>
2021-06-18 17:42:08 +0200

    Update g_Serial.ino

--- Software/g_Serial.ino
+++ Software/g_Serial.ino
@@ -9,18 +9,20 @@ void serialRun()
     switch (serialPage)
     {
       case 'E':   // Energy plot
-        Console1.printf("Vset:%06.3f Vout:%06.3f Iset:%06.3f Iout:%06.3f Wout:%+07.3f\n", dashboard.Vset, dashboard.Vout, dashboard.Iset, dashboard.Iout, dashboard.Wout);
+        Console1.printf("SetVout:%06.3f Vout:%06.3f SetIout:%06.3f Iout:%06.3f Wout:%+07.3f\n", dashboard.SetVout, dashboard.Vout, dashboard.SetIout, dashboard.Iout, dashboard.Wout);
         if (serialPeriodicity == '!') serialPage = 0; // One shot reset serial page.
         break;
       case 'D':   // Debug Report
-        Console1.printf("ADC_VinRaw:%u Vin:%06.3f ADC_VoutRaw:%u Vout:%06.3f ADC_IoutRaw:%u Iout:%06.3f PWM_Vset:%04i Vset:%06.3f PWM_Cset:%04i Iset:%06.3f \n" , ADC_VinRaw, dashboard.Vin, ADC_VoutRaw, dashboard.Vout, ADC_IoutRaw, dashboard.Iout, PWM_Vset, dashboard.Vset, PWM_Cset, dashboard.Iset);
+        // Console1.printf("ADC_VinRaw:%i Vin:%06.3f ADC_IinRaw:%i Iin:%06.3f \nADC_VoutRaw:%i Vout:%06.3f ADC_IoutRaw:%i Iout:%06.3f PWM_SetVout:%04i SetVout:%06.3f PWM_SetIout:%04i SetIout:%06.3f PWM_Fan:%04i \n" , ADC_VinRaw, dashboard.Vin, ADC_IinRaw, dashboard.Iin, ADC_VoutRaw, dashboard.Vout, ADC_IoutRaw, dashboard.Iout, PWM_SetVout, dashboard.SetVout, PWM_SetIout, dashboard.SetIout, PWM_Fan);
+        // Console1.printf("Vin:%06.3f SetVin:%06.3f Iin:%06.3f Vout:%06.3f SetVout:%06.3f Iout:%06.3f SetIout:%06.3f ConIout:%06.3f I_Value %06.3f  CtrlMode:%02i\n" , dashboard.Vin, dashboard.SetVin, dashboard.Iin, dashboard.Vout, dashboard.SetVout, dashboard.Iout, dashboard.SetIout, dashboard.ConIout, I_value, dashboard.CtrlMode );
+        Console1.printf("Vin:%06.3f SetVin:%06.3f Iout:%06.3f ConIout:%06.3f I_Value %06.3f\n" , dashboard.Vin, dashboard.SetVin, dashboard.Iout, dashboard.ConIout, I_value);
         if (serialPeriodicity == '!') serialPage = 0; // One shot reset serial page.
         break;
       case 'S': // Summary
         sprintf(charbuff, " \nSummary Report for \n%s, %02d %s %04d ", DayName , Day , MonthName, Year);
         Console1.print(charbuff);
-        Console1.printf("\nHour|    Ah   |    Vh    |    Wh    |\n");
-        for  (byte n = 0; n < 31; n++)
+        Console1.printf("\nHour|    Ah   |    VoutAvg    |    Wh    |\n");
+        for  (byte n = 0; n < 32; n++)
         {
           if (n == 24)
           {
@@ -28,7 +30,7 @@ void serialRun()
           }
           else
           {
-            Console1.printf("%02u  | %+07.3f | %+07.3f | %+07.3f |\n", n, Ah[n], Vh[n], Ah[n] * Wh[n]);
+            Console1.printf("%02u  | %+07.3f | %+07.3f | %+07.3f |\n", n, Ah[n], VoutAvg[n], Ah[n] * Wh[n]);
           }
         }
         serialPage = 0;
@@ -40,5 +42,5 @@ void serialRun()
     }  // end switch (serialPage)
     
   }
-  //          console.printf("Vset:%06.3f Vout:%06.3f Iset:%06.3f Iout:%06.3f Wout:%+07.3f\n", dashboard.Vset, dashboard.Vout, dashboard.Iset, dashboard.Iout, dashboard.Wout);
+  //          console.printf("SetVout:%06.3f Vout:%06.3f SetIout:%06.3f Iout:%06.3f Wout:%+07.3f\n", dashboard.SetVout, dashboard.Vout, dashboard.SetIout, dashboard.Iout, dashboard.Wout);
 }
