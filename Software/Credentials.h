163991018f36a7e4f477147a5c86febfe7f6a729
RIN67630 <lazlo.lebrun@googlemail.com>
2021-06-27 14:21:38 +0200

    Update Credentials.h

--- Software/Credentials.h
+++ Software/Credentials.h
@@ -6,6 +6,12 @@
 #define WIFI_PASS          "your password"
 
 //Thinger
-#define THINGER_USERNAME           "your user name"       
+// #define THINGER_USERNAME           "your user name"       // moved to Options
 #define THINGER_DEVICE_CREDENTIALS "your device credentials"  //NOT the website password!
 #define THINGER_DEVICE DEVICE_NAME  
+
+//OpenWeatherMap
+#define OPEN_WEATHER_MAP_APP_ID      "OWM APP ID"
+#define OPEN_WEATHER_MAP_LOCATION_ID "OWM LOCATION ID"
+#define OPEN_WEATHER_MAP_LANGUAGE    "en"
+#define OPEN_WEATHER_MAP_UNITS       "metric"
